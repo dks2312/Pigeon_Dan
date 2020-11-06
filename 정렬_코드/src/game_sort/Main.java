7ef3a9e4a11f7f1c02887b45ab13181940ade676
fkdlalsem <as5869bc@gmail.com>
2020-11-07 00:24:50 +0900

    sort_program

---
+++ 정렬_코드/src/game_sort/Main.java
@@ -0,0 +1,25 @@
+package game_sort;
+
+import java.util.Scanner;
+public class Main {
+	static int numbers=0;
+	static void ranking(int score) {
+		// TODO Auto-generated method stub
+		int i;
+		
+		numbers+=1;											//~번 지나갔음을 알림
+		char[] element = new char[numbers];					//~번 도는 배열을 생성
+		for(i=0;i<numbers;i++)
+		{
+		element[i] = (char)score;							//배열에 숫자 저장
+		}
+		
+		if(numbers==1) {
+			System.out.printf("%d", score);					//만약 처음으로 숫자 넣으면 1출력
+		}
+		
+		Sort.bubbling(element);
+	}
+		
+}
+
